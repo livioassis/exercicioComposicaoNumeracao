dd3d25fe226d88f5c9b122a9349d784b583f1a69
Livio Assis <livioassis@yahoo.com.br>
2019-11-20 21:15:25 -0300

    first commit

---
+++ exercicioComposicaoEnumeracao/Entities/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercicioComposicaoEnumeracao.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string EMail{ get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string eMail, DateTime birthDate)
+        {
+            Name = name;
+            EMail = eMail;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return
+                Name +
+                " (" +
+                BirthDate.ToString("dd/MM/yyyy")
+                + ") - " +
+                EMail;
+        }
+    }
+}
---
+++ exercicioComposicaoEnumeracao/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using exercicioComposicaoEnumeracao.Entities;
+using exercicioComposicaoEnumeracao.Entities.Enums;
+
+namespace exercicioComposicaoEnumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine().ToString());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string prdName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prdPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(prdName, prdPrice);
+
+                Console.Write("Quantity: ");
+                int prdQuantity = int.Parse(Console.ReadLine());
+
+                OrderItem item = new OrderItem(prdQuantity, prdPrice, product);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine("Order items:");
+            Console.WriteLine(order);
+        }
+    }
+}
